6e048a6ec7805a1d891281e31b337eb971020a5e
HutchisonSorbo <ryanhutchison@outlook.com.au>
2025-12-13 20:30:28 +1100

    Debug: Enable logging for cookie set errors

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -19,6 +19,7 @@ export async function createClient() {
                         // The `set` method was called from a Server Component.
                         // This can be ignored if you have middleware refreshing
                         // user sessions.
+                        console.error('Error setting cookie', name, error)
                     }
                 },
                 remove(name: string, options: CookieOptions) {
