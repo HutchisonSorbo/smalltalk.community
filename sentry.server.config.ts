13a9a2c607b9952639c2ea604c215c68e203e8db
HutchisonSorbo <ryanhutchison@outlook.com.au>
2025-12-22 20:38:41 +1100

    feat: integrate Sentry monitoring and adding .cursorrules

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1542caab2225bdce6511f8e92e8a862e@o4510577777770496.ingest.de.sentry.io/4510577786683472",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
