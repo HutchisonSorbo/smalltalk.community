67fb9fc0ddbe6ab6c6048b851ef1abed6fb691de
HutchisonSorbo <ryanhutchison@outlook.com.au>
2025-12-29 14:19:27 +1100

    fix(csp): allow map tile and icon domains

--- next.config.mjs
+++ next.config.mjs
@@ -53,7 +53,7 @@ const nextConfig = {
                     },
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://jgtgvxzxgkudctocoxsq.supabase.co https://images.unsplash.com; font-src 'self' data:; connect-src 'self' https://jgtgvxzxgkudctocoxsq.supabase.co https://*.sentry.io; frame-ancestors 'none'; upgrade-insecure-requests"
+                        value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://jgtgvxzxgkudctocoxsq.supabase.co https://images.unsplash.com https://*.tile.openstreetmap.org https://unpkg.com https://raw.githubusercontent.com https://cdnjs.cloudflare.com; font-src 'self' data:; connect-src 'self' https://jgtgvxzxgkudctocoxsq.supabase.co https://*.sentry.io; frame-ancestors 'none'; upgrade-insecure-requests"
                     }
                 ]
             }
